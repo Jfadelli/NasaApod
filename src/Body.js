84af7da685da50c46d561dcf59bdb4e3cd85f14b
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-06-10 16:18:51 -0700

    mvp complete

---
+++ src/Body.js
@@ -0,0 +1,63 @@
+import React, { Children } from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: row;
+align-items: flex-start;;
+align-content: flex-start;
+justify-content: space-evenly;
+// margin: 2% 7%;
+background-color: black;
+color:white;
+`
+
+const ImgDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: calc(50% - 20px);
+margin: 10px;
+border 2px dashed white;
+border-radius 6px;
+`
+const StyledImg = styled.img`
+display: block;
+object-fit: contain;
+width: 100%;
+height: 100%;
+padding: 10px 10px 0px 10px
+`
+const InfoDiv = styled.div`
+display: flex
+flex-direction: column;
+max-width: 40%;
+`
+
+const StyledH1 = styled.h1`
+
+`
+
+const StyledP = styled.p`
+line-height: 0em
+`
+
+function Body(props) {
+    console.log(props.data)
+    return (
+        <StyledDiv>
+            <InfoDiv>
+                <StyledH1>{props.data.title}</StyledH1>
+
+                <p>{props.data.explanation}</p>
+            </InfoDiv>
+            <ImgDiv>
+                <StyledImg src={props.data.url} alt="apod from nasa" />
+                <StyledP>{props.data.copyright}</StyledP>
+                <StyledP>{props.data.date}</StyledP>
+            </ImgDiv>
+        </StyledDiv>
+    )
+}
+export default Body
